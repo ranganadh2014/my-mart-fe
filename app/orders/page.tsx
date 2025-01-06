52cf422ab6b9f01339bac03353c02134fa933445
Ranganadh Thatha <ranganadh.thatha@gmail.com>
2025-01-06 20:28:43 +0530

    orders page code cleanup

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,5 +1,4 @@
 import getOrders from "./actions/get-orders";
-// import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,29 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-// const StyledTableCell = styled(TableCell)(({ theme }) => ({
-//     [`&.${tableCellClasses.head}`]: {
-//       backgroundColor: theme.palette.common.black,
-//       color: theme.palette.common.white,
-//     },
-//     [`&.${tableCellClasses.body}`]: {
-//       fontSize: 14,
-//     },
-//   }));
-
-// const StyledTableRow = styled(TableRow)(({ theme }) => ({
-//     '&:nth-of-type(odd)': {
-//       backgroundColor: theme.palette.action.hover,
-//     },
-//     // hide last border
-//     '&:last-child td, &:last-child th': {
-//       border: 0,
-//     },
-//   }));
-
 export default async function Orders() {
     const ordersList = await getOrders();
-    console.log(ordersList);
 
     return (
         <>
